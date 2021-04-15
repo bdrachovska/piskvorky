f568d5ea22b0368359d7ca3a5955bdd237d3db0a
Barbora Drachovská <bdrachovska@gmail.com>
2021-04-15 09:14:56 +0200

    Ukol 4 - javascript, doplneni

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 'use-strict';
 let hraje = 'circle';
+const hrajeElm = document.querySelector('#krizky_kolecka');
 
 const naTahu = (event) => {
   event.target.classList.add('board__field--' + hraje);
   if (hraje === 'circle') {
     hraje = 'cross';
+    hrajeElm.src = 'obrazky/cross.svg';
+    hrajeElm.alt = 'Na tahu křížek.';
   } else {
     hraje = 'circle';
+    hrajeElm.src = 'obrazky/circle.svg';
+    hrajeElm.alt = 'Na tahu kolečko.';
   }
   event.target.disabled = true;
 };

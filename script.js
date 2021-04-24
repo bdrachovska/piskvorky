9217261f0e77be75b3bc68db1b6f2f0582711f2c
Barbora Drachovská <bdrachovska@gmail.com>
2021-04-24 18:08:33 +0200

    ukol5 - javascript

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 'use-strict';
+
+/** ukol 5 */
+
+const boardSize = 10;
+const fields = document.querySelectorAll('.policko__hra');
+
+const getPosition = (field) => {
+  let fieldIndex = 0;
+  while (fieldIndex < fields.length) {
+    if (field === fields[fieldIndex]) {
+      break;
+    }
+    fieldIndex++;
+  }
+
+  return {
+    row: Math.floor(fieldIndex / boardSize),
+    column: fieldIndex % boardSize,
+  };
+};
+console.log(getPosition(fields[35]));
+
+const getField = (row, column) => fields[row * boardSize + column];
+
+const getSymbol = (field) => {
+  if (field.classList.contains('board__field--cross')) {
+    return 'cross';
+  } else if (field.classList.contains('board__field--circle')) {
+    return 'circle';
+  }
+};
+/**ukol 4*/
 let hraje = 'circle';
 const hrajeElm = document.querySelector('#krizky_kolecka');
 
@@ -19,4 +51,70 @@ const naTahu = (event) => {
 const tlacitkoHra = document.querySelectorAll('button');
 for (let i = 0; i < tlacitkoHra.length; i += 1) {
   tlacitkoHra[i].addEventListener('click', naTahu);
+}
+/**ukol 4 - konec*/
+
+const symbolsToWin = 5;
+const isWinningMove = (field) => {
+  const origin = getPosition(field);
+  const symbol = getSymbol(field);
+
+  let i;
+
+  let inRow = 1;
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    inRow++;
+    i--;
+  }
+
+  i = origin.column;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(origin.row, i + 1))
+  ) {
+    inRow++;
+    i++;
+  }
+
+  if (inRow >= symbolsToWin) {
+    return true;
+  }
+
+  let inColumn = 1;
+  i = origin.row;
+  while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+    inColumn++;
+    i--;
+  }
+
+  i = origin.row;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(i + 1, origin.column))
+  ) {
+    inColumn++;
+    i++;
+  }
+
+  if (inColumn >= symbolsToWin) {
+    return true;
+  }
+
+  return false;
+};
+
+const winning = (event) => isWinningMove(e.target);
+{
+  if (winning === true && getSymbol === 'circle') {
+    let alert = `Toto kolo zvítězil kroužek.`;
+    if (alert === true) {
+      location.href('hra.html');
+    } else if (winning === true && getSymbol === 'cross') {
+      let alert = `Toto kolo zvítězil křížek.`;
+      if (alert === true) {
+        location.href('hra.html');
+      }
+    }
+  }
 }
